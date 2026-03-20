91912469ee2cab40610c27048e357ef5dec87da7
lilyhigg <113061817+lilyhigg@users.noreply.github.com>
2026-03-20 14:33:52 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+// function hidejunior(){
+//     var jr = document.querySelectorAll(".jr")
+//     for (i = 0;i< jr.length;i++){
+//         console.log("Here")
+//         jr[i].style.display = "none" 
+//     }
+// }
+
+function checkphoto(element){
+    element.src='https://lilyhigg.github.io/338mobile/images/skyline_logo.png'
+    element.alt="''"
+    element.style.width="25%"
+    element.style.margin="auto"
+    element.style.display="block"
+}
